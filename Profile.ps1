7555aaa65dc1d486ff5562dc4fdc54f08cc1485a
bighiller <35498997+bighiller@users.noreply.github.com>
2019-11-12 12:35:34 -0800

    Update Profile.ps1

--- Profile.ps1
+++ Profile.ps1
@@ -5,8 +5,9 @@ function Connect-O365
 #Check for MSOLService module
 #Check for Exchange Online module
 #Check for Azure AD module
+If(!(get-module | where name -eq "Microsoft.Exchange.Management.ExoPowershellModule"))
+{Import-Module $((Get-ChildItem -Path $($env:LOCALAPPDATA+"\Apps\2.0\") -Filter Microsoft.Exchange.Management.ExoPowershellModule.dll -Recurse ).FullName|?{$_ -notmatch "_none_"}|select -First 1)}
 Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\WinRM\Client" -Name "AllowBasic" -Value 1 | out-null
-Import-Module $((Get-ChildItem -Path $($env:LOCALAPPDATA+"\Apps\2.0\") -Filter Microsoft.Exchange.Management.ExoPowershellModule.dll -Recurse ).FullName|?{$_ -notmatch "_none_"}|select -First 1)
 Connect-EXOPSSession
 Connect-MsolService
 }
