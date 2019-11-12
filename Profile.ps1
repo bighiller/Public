96a649961ab5404d64ccfe1b378ccb43c66ddaf9
bighiller <35498997+bighiller@users.noreply.github.com>
2019-11-12 12:33:53 -0800

    Update Profile.ps1

--- Profile.ps1
+++ Profile.ps1
@@ -1,12 +1,12 @@
-#My Profile
+
 function Connect-O365 
 {
+#requires -RunAsAdministrator
 #Check for MSOLService module
 #Check for Exchange Online module
 #Check for Azure AD module
 Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\WinRM\Client" -Name "AllowBasic" -Value 1 | out-null
 Import-Module $((Get-ChildItem -Path $($env:LOCALAPPDATA+"\Apps\2.0\") -Filter Microsoft.Exchange.Management.ExoPowershellModule.dll -Recurse ).FullName|?{$_ -notmatch "_none_"}|select -First 1)
-$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "https://ps.outlook.com/powershell/" -Authentication Basic -AllowRedirection
-Import-PSSession $Session
+Connect-EXOPSSession
 Connect-MsolService
 }
