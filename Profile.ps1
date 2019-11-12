cc703435272df2dfcef0a170a7601514d2417673
bighiller <35498997+bighiller@users.noreply.github.com>
2019-11-12 12:18:17 -0800

    Update Profile.ps1

--- Profile.ps1
+++ Profile.ps1
@@ -2,8 +2,7 @@
 function Connect-O365 
 {
 Import-Module $((Get-ChildItem -Path $($env:LOCALAPPDATA+"\Apps\2.0\") -Filter Microsoft.Exchange.Management.ExoPowershellModule.dll -Recurse ).FullName|?{$_ -notmatch "_none_"}|select -First 1)
-$UserCredential = Get-Credential
-$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "https://ps.outlook.com/powershell/" -Credential $UserCredential -Authentication Basic -AllowRedirection
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "https://ps.outlook.com/powershell/" -Authentication Basic -AllowRedirection
 Import-PSSession $Session
-Connect-MsolService -Credential $UserCredential
+Connect-MsolService
 }
